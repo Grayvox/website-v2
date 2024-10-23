edb8d84658e7efd90f9a468a2f39341779a567b6
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-10-23 12:29:01 -0400

    feat: Add social links and titles on home page

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,11 @@
 
 export const SITE_TITLE = 'Grayvox';
 export const SITE_DESCRIPTION = 'Welcome to my little corner on the internet.';
+
+export const LINKS = {
+    youtube: 'https://www.youtube.com/@Grayvox',
+    twitch: 'https://www.twitch.tv/grayvoxdev',
+    github: 'https://www.github.com/Grayvox',
+    kofi: 'https://www.ko-fi.com/grayvox',
+    linktree: 'https://www.linktr.ee/grayvox'
+};
