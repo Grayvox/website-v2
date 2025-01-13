5277855fae9830d33943f3f8c01b424ef13ac964
Grayvox <144566632+Grayvox@users.noreply.github.com>
2025-01-13 01:22:02 -0500

    Remove unneeded comments

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'Grayvox';
 export const SITE_DESCRIPTION = 'Welcome to my little corner on the internet.';
 
